e162a40b6a92d23dded9942df4d6584a7bcaff6f
codymerritt347 <cody.merritt@icloud.com>
2021-01-29 16:55:02 -0500

    commit tree d0b3e42f97dd107faef3490fefcbccbbc995a0da

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,17 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :new
+    end
 
+    post '/teams' do
+      @team = Team.new(params[:team])
+
+      params[:team][:heroes].each do |hero_hash|
+        Hero.new(hero_hash)
+      end
+      @heroes = Hero.all
+      erb :team
+    end
 end
---
+++ app/models/hero.rb
@@ -0,0 +1,17 @@
+class Hero
+  
+  attr_accessor :name, :power, :biography
+  @@heroes = []
+
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @biography = params[:biography]
+    @@heroes << self
+  end
+
+  def self.all
+    @@heroes
+  end
+
+end
